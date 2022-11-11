061289844e28ddc64657f598d1bae4ae457e93ab
Yohani95 <yohani95301@gmail.com>
2022-11-11 15:12:27 -0300

    avances en login y base de datos

---
+++ database/migrations/2022_11_11_180018_products.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->engine="InnoDB";
+            $table->id();
+            $table->bigInteger('categories_id')->unsigned();
+            $table->string('name');
+            $table->timestamps();
+            $table->foreign('categories_id')->references('id')->on('categories')->ondelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};

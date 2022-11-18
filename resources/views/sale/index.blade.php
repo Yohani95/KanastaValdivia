50b0b5bb94a33592944b811f5524065a46822d1d
ErnestoM22 <86693541+ErnestoM22@users.noreply.github.com>
2022-11-17 22:08:46 -0300

    Avances

--- resources/views/sale/index.blade.php
+++ resources/views/sale/index.blade.php
@@ -54,11 +54,11 @@
 
                                             <td>
                                                 <form action="{{ route('sales.destroy',$sale->id) }}" method="POST">
-                                                    <a class="btn btn-sm btn-primary " href="{{ route('sales.show',$sale->id) }}"><i class="fa fa-fw fa-eye"></i> Show</a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('sales.edit',$sale->id) }}"><i class="fa fa-fw fa-edit"></i> Edit</a>
+                                                    <a class="btn btn-sm btn-primary " href="{{ route('sales.show',$sale->id) }}"><i class="fa fa-fw fa-eye"></i> Ver</a>
+                                                    <a class="btn btn-sm btn-success" href="{{ route('sales.edit',$sale->id) }}"><i class="fa fa-fw fa-edit"></i> Editar</a>
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i> Delete</button>
+                                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i> Eliminar</button>
                                                 </form>
                                             </td>
                                         </tr>
--- resources/views/voucher/index.blade.php
+++ resources/views/voucher/index.blade.php
@@ -52,11 +52,11 @@
 
                                             <td>
                                                 <form action="{{ route('vouchers.destroy',$voucher->id) }}" method="POST">
-                                                    <a class="btn btn-sm btn-primary " href="{{ route('vouchers.show',$voucher->id) }}"><i class="fa fa-fw fa-eye"></i> Show</a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('vouchers.edit',$voucher->id) }}"><i class="fa fa-fw fa-edit"></i> Edit</a>
+                                                    <a class="btn btn-sm btn-primary " href="{{ route('vouchers.show',$voucher->id) }}"><i class="fa fa-fw fa-eye"></i> ver</a>
+                                                    <a class="btn btn-sm btn-success" href="{{ route('vouchers.edit',$voucher->id) }}"><i class="fa fa-fw fa-edit"></i> Editar</a>
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i> Delete</button>
+                                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i> Eliminar</button>
                                                 </form>
                                             </td>
                                         </tr>

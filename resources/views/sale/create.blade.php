802f4a539f4fc7aecf84544a92a863dec74a8225
ErnestoM22 <86693541+ErnestoM22@users.noreply.github.com>
2022-11-22 20:44:57 -0300

    Cambios

--- resources/views/sale/create.blade.php
+++ resources/views/sale/create.blade.php
@@ -13,7 +13,7 @@
 
                 <div class="card card-default">
                     <div class="card-header">
-                        <span class="card-title">Create Sale</span>
+                        <span class="card-title">Crear Tienda</span>
                     </div>
                     <div class="card-body">
                         <form method="POST" action="{{ route('sales.store') }}"  role="form" enctype="multipart/form-data">
--- resources/views/sale/form.blade.php
+++ resources/views/sale/form.blade.php
@@ -2,8 +2,8 @@
     <div class="box-body">
         
         <div class="form-group">
-            {{ Form::label('name') }}
-            {{ Form::text('name', $sale->name, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => 'Name']) }}
+            {{ Form::label('nombre') }}
+            {{ Form::text('nombre', $sale->name, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => 'Name']) }}
             {!! $errors->first('name', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         <div class="form-group">
@@ -12,13 +12,13 @@
             {!! $errors->first('direcction', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         <div class="form-group">
-            {{ Form::label('phone') }}
-            {{ Form::text('phone', $sale->phone, ['class' => 'form-control' . ($errors->has('phone') ? ' is-invalid' : ''), 'placeholder' => 'Phone']) }}
+            {{ Form::label('telefono') }}
+            {{ Form::text('telefono', $sale->phone, ['class' => 'form-control' . ($errors->has('phone') ? ' is-invalid' : ''), 'placeholder' => 'Phone']) }}
             {!! $errors->first('phone', '<div class="invalid-feedback">:message</div>') !!}
         </div>
 
     </div>
     <div class="box-footer mt20">
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Aceptar</button>
     </div>
 </div>

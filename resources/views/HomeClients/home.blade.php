5bcaadfadf9bf3af40ac5bedd6349669ef840b95
ErnestoM22 <86693541+ErnestoM22@users.noreply.github.com>
2022-11-30 23:47:16 -0300

    Avance

--- resources/views/HomeClients/home.blade.php
+++ resources/views/HomeClients/home.blade.php
@@ -34,28 +34,12 @@
                             <strong>Stock:</strong>
                             {{ $product->stock }}
                         </div>
-
-
-
-
-                        <!-- cart details -->
-<div class="col-2 top_nav_right text-center mt-sm-0 mt-2">
-                                <div class="wthreecartaits wthreecartaits2 cart cart box_1">
-                                    <form action="#" method="post" class="last">
-                                        <input type="hidden" name="cmd" value="_cart">
-                                        <input type="hidden" name="display" value="1">
-                                        <button class="btn w3view-cart" type="submit" name="submit" value="">
-                                            <i class="fas fa-cart-arrow-down"></i>
-                                        </button>
-                                    </form>
-                                </div>
-                            </div>
-                            <!-- //cart details -->
-                </p>
+                        </p>
                  <a href="{{asset($product->ruta_image)}}" class="btn btn-primary">Ver Producto</a>
                  <br>
                  <br>
                  <a href="{{asset($product->ruta_image)}}" class="btn btn-primary">Agregar al carro</a>
+               
                 </div>
             </div>
         </div>

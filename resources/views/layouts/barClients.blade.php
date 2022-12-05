0c81df28b2d76f441fe2b8f00bd0575389ee39b4
ErnestoM22 <86693541+ErnestoM22@users.noreply.github.com>
2022-12-05 18:43:41 -0300

    Cambios

--- resources/views/layouts/barClients.blade.php
+++ resources/views/layouts/barClients.blade.php
@@ -42,7 +42,7 @@
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Categorias
+            Categorias de productos
           </a>
           <ul class="dropdown-menu">
           @foreach ($categories as $category)
